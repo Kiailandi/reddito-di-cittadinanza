f1cc638d20b2e99e4db89bf7aaad853998572bc5
Tommaso Montefusco <tommaso.montefusco@hotmail.it>
2018-05-11 01:22:21 +0200

    Add some love

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const loadCounter = () => {
     const startDay = moment('2018-05-10', 'YYYY-MM-DD');
     const now = moment();
     document.getElementById('app').innerHTML = `<h1 class="flex-item">Sono passati: ⏱️ <b>${now.diff(startDay, 'days')} giorni</b> ️️⏱️</h1><h2 class="flex-item">e siamo ancora senza 💰 <b>REDDITO DI CITTADINANZA</b> 💰</h2>`;
+    document.getElementById('app').innerHTML += `<h3 class="flex-item-footer">Made with <span class="heart">❤</span> by <a href="http://kiailandi.github.io">Kiailandi</a></h3>`;
 };
 
 if( document.readyState === 'complete' ) {
