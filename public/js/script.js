7eede5fd79b0457b2ae8b62c894ec46378f0ce29
Tommaso Montefusco <tommaso.montefusco@hotmail.it>
2018-05-11 00:43:44 +0200

    Add version 1.0

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+const loadCounter = () => {
+    const startDay = moment('2018-05-10', 'YYYY-MM-DD');
+    const now = moment();
+    document.getElementById('app').innerHTML = `<h1 class="flex-item">Sono passati: ⏱️ <b>${now.diff(startDay, 'days')} giorni</b> ️️⏱️</h1><h2 class="flex-item">e siamo ancora senza 💰 <b>REDDITO DI CITTADINANZA</b> 💰</h2>`;
+};
+
+if( document.readyState === 'complete' ) {
+    loadCounter();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        loadCounter();
+    });
+}
